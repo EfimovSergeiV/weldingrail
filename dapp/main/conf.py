b86ac8bbe7b3ba79bdab34eecc7a02234fc304ff
anon <efimovsergeiv@gmail.com>
2024-04-16 10:05:47 +0300

    add:

--- dapp/main/conf.py
+++ dapp/main/conf.py
@@ -1,1 +1,5 @@
-qwe
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-_37p#d600&%z-^lpcy&81dd339&t#34#n&m3%)$h8a7w7#xkb+'
--- dapp/main/settings.py
+++ dapp/main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
+from main.conf import *
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
