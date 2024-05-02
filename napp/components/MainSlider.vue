151356c579d3f88fce624e33a2b3aab96990f8db
anon <efimovsergeiv@gmail.com>
2024-05-02 17:18:54 +0300

    add:

--- napp/components/MainSlider.vue
+++ napp/components/MainSlider.vue
@@ -1,13 +1,61 @@
 <script setup>
-	const swiper = useSwiper()
+import { TransitionGroup } from 'vue';
 
+
+
+  const currentSlide = ref(0)
+
+
+  const onSlideChange = (swiper) => {
+    currentSlide.value = swiper.activeIndex
+  };
+
+
+  const slides = [
+    { "id": 1, "image": "/slides/1.webp", "toptext": "1 All types of high quality plastic materials which are used in the worlds most modern fastest", "bottomtext": "paper machines are manufactured on our Jäger weaving machines. " },
+    { "id": 2, "image": "/slides/2.webp", "toptext": "2 All types of high quality plastic materials which are used in the worlds most modern fastest", "bottomtext": "paper machines are manufactured on our Jäger weaving machines. " },
+    { "id": 3, "image": "/slides/3.webp", "toptext": "3 All types of high quality plastic materials which are used in the worlds most modern fastest", "bottomtext": "paper machines are manufactured on our Jäger weaving machines. " },
+    { "id": 4, "image": "/slides/4.webp", "toptext": "4 All types of high quality plastic materials which are used in the worlds most modern fastest", "bottomtext": "paper machines are manufactured on our Jäger weaving machines. " },
+  ]
+
+
+  const showSlideData = ref([])
 
   const onSwiper = (swiper) => {
-    console.log(swiper.activeIndex);
+    let text = slides[0]
+
+    showSlideData.value = []
+
+    /// с задержкой в 500 мсек добавляем текст в массив
+    setTimeout(() => {
+      showSlideData.value.push(text.toptext)
+      
+    }, 500)
+    setTimeout(() => {
+      showSlideData.value.push(text.bottomtext)
+    }, 1000)
   };
-  const onSlideChange = (swiper) => {
-    console.log('slide change', swiper.activeIndex);
-  };
+
+  watch(currentSlide, (newVal) => {
+
+    let text = slides[newVal]
+
+    showSlideData.value = []
+
+    /// с задержкой в 500 мсек добавляем текст в массив
+    setTimeout(() => {
+      showSlideData.value.push(text.toptext)
+      
+    }, 500)
+    setTimeout(() => {
+      showSlideData.value.push(text.bottomtext)
+    }, 1000)
+
+    // showSlideData.value.push(text.toptext)
+    // showSlideData.value.push(text.bottomtext)
+
+    
+  })
 
 
 </script>
@@ -33,9 +81,9 @@
         }"
       >
       
-        <SwiperSlide v-for="slide, pk in ['/slides/1.webp','/slides/2.webp','/slides/3.webp','/slides/4.webp',]" :key="pk">
+        <SwiperSlide v-for="slide in slides" :key="slide.id">
           <div class="">
-            <img :src="slide" class="" />
+            <img :src="slide.image" class="" />
           </div>
         </SwiperSlide>
 
@@ -56,5 +104,49 @@
 
       </Swiper>
 
+      <div class="absolute left-0 bottom-1/2 z-40 p-3">
+        
+        <transition-group tag="div" name="list">
+          
+          <div v-for="slide, pk in showSlideData" :key="pk" >
+            <div :id="pk" class="bg-white p-2 my-2">
+              <p>{{ slide }}</p>
+            </div>
+          </div>
+
+        </transition-group>
+        
+      </div>
+
     </div>
 </template>
+
+
+<style>
+  .swiper-pagination {
+    position: absolute;
+    text-align: left;
+    margin-left: 6vw;
+    transition: 300ms opacity;
+    transform: translate3d(0, 0, 0);
+    z-index: 10;
+  }
+  .swiper-pagination-bullet {
+    width: 18px;
+    width: var(--swiper-pagination-bullet-width, var(--swiper-pagination-bullet-size, 18px));
+    height: 18px;
+    height: var(--swiper-pagination-bullet-height, var(--swiper-pagination-bullet-size, 18px));
+    display: inline-block;
+    border-radius: 50%;
+    border-radius: var(--swiper-pagination-bullet-border-radius, 50%);
+    background: #0369a1;
+    background: var(--swiper-pagination-bullet-inactive-color, #0369a1);
+    opacity: 0.5;
+    opacity: var(--swiper-pagination-bullet-inactive-opacity, 0.5);
+  }
+  .swiper-pagination-bullet-active {
+    opacity: 1;
+    opacity: var(--swiper-pagination-bullet-opacity, 1);
+    background: #082f49;
+  }
+</style>
--- napp/pages/index.vue
+++ napp/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+  console.log('index page');
 
 </script>
 
@@ -32,31 +32,3 @@
 </template>
 
 
-<style>
-  .swiper-pagination {
-    position: absolute;
-    text-align: left;
-    margin-left: 6vw;
-    transition: 300ms opacity;
-    transform: translate3d(0, 0, 0);
-    z-index: 10;
-  }
-  .swiper-pagination-bullet {
-    width: 18px;
-    width: var(--swiper-pagination-bullet-width, var(--swiper-pagination-bullet-size, 18px));
-    height: 18px;
-    height: var(--swiper-pagination-bullet-height, var(--swiper-pagination-bullet-size, 18px));
-    display: inline-block;
-    border-radius: 50%;
-    border-radius: var(--swiper-pagination-bullet-border-radius, 50%);
-    background: #0369a1;
-    background: var(--swiper-pagination-bullet-inactive-color, #0369a1);
-    opacity: 0.5;
-    opacity: var(--swiper-pagination-bullet-inactive-opacity, 0.5);
-  }
-  .swiper-pagination-bullet-active {
-    opacity: 1;
-    opacity: var(--swiper-pagination-bullet-opacity, 1);
-    background: #082f49;
-  }
-</style>
