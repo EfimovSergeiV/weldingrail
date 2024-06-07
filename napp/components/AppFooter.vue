3c6b945c0d79478d806295a3658351bbc8484b74
anon <efimovsergeiv@gmail.com>
2024-06-07 12:51:30 +0300

    add:

--- napp/components/AppFooter.vue
+++ napp/components/AppFooter.vue
@@ -28,35 +28,40 @@
           <div class="flex flex-wrap items-start gap-14 px-8">
             <div class="">
               <div class="pb-2">
-                <p class="text-white uppercase font-semibold text-lg italic">{{ $t('RWQ')}}</p>
+                <p class="text-white uppercase  font-semibold text-lg italic border-b border-white/50">{{ $t('RWQ')}}</p>
               </div>
               <div class="grid grid-cols-1 gap-y-2 gap-x-14">
                 <div v-for="ct in props.categories" class="">
-                  <p class="text-white">{{ ct.name }}</p>
+                  <p class="text-gray-300">{{ ct.name }}</p>
                 </div>
               </div>
             </div>
             <div class="">
               <div class="pb-2">
-                <p class="text-white uppercase font-semibold text-lg italic">{{ $t('components')}}</p>
+                <p class="text-white uppercase font-semibold text-lg italic border-b border-white/50">{{ $t('components')}}</p>
               </div>
               <div class="grid grid-cols-1 gap-y-2 gap-x-14">
                 <div v-for="sct in props.subcategories" class="">
-                  <p class="text-white">{{ sct.name }}</p>
+                  <p class="text-gray-300">{{ sct.name }}</p>
                 </div>
               </div>
             </div>
           </div>
           <div class="">
-            <div class="bg-white bg-slice-left px-8 py-2">
-              <p class="text-sky-900 text-lg font-bold uppercase italic text-center">{{ $t('subscriebe') }}</p>
+            <div class="bg-sky-800 bg-slice-left border-y  border-white/20 px-4 py-6">
+              <p class="text-white text-lg font-bold uppercase italic text-center">{{ $t('subscriebe') }}</p>
 
-              <div id="subscriebe" class="flex items-center gap-0.5 py-2 pl-0.5">
+              <div id="subscriebe" class="flex items-center gap-4 py-2 pl-4">
                 <div class="border-b border-sky-900">
                   <input type="email" style="font-family: Play;" class="w-[22vw] text-center h-8 px-4 text-base text-sky-900 border border-white/0 focus:border-white/0 focus:ring-gray-600/0 ring-0 placeholder-sky-800" :placeholder="$t('contactus.email')" /> 
                 </div>
-                <button class="h-8 px-4 text-base uppercase bg-white text-sky-900 shadow-xl shadow-gray-900/10 mdi mdi-send-variant"></button>
-              </div>              
+
+              </div>
+              <div class="flex items-center justify-center mt-2">
+                <button class="h-8 px-14 text-base uppercase bg-sky-900 text-white ">
+                  <p class="">{{ $t('send') }}</p>
+                </button>                   
+              </div>
             </div>
 
 
