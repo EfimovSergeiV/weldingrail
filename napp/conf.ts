ac12b5245f9c7591add7be11a46ff4c873266511
anon <efimovsergeiv@gmail.com>
2024-06-19 13:04:46 +0300

    add:

--- napp/conf.ts
+++
@@ -1,8 +0,0 @@
-const cfg = {
-    "backend": 'http://192.168.60.250:8090/',   //// 'http://127.0.0.1:8000' 'http://192.168.60.250:8080/', ///'http://192.168.60.201:8080/', 'http://127.0.0.1:8090/'
-    "gtmId": '',
-    "yandexMetrika": '',
-    "googleAnalyticsId": '',
-  }
-  
-  export default cfg
